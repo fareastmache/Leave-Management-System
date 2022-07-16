42806f35248daafa996a67167de64a18fb851e61
fareastmache <54427696+fareastmache@users.noreply.github.com>
2022-07-16 21:06:02 +0800

    added button spinner

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.auth')
 
+@section('style')
+    <style>
+        .hide {
+            display: none;
+        }
+    </style>
+@endsection
+
 @section('content')
     <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                    
+
                     <!-- Start Logo --> 
                     <div class="d-flex justify-content-center py-4">
                         <a href="{{ route('login') }}" class="logo d-flex align-items-center w-auto">
@@ -26,6 +34,14 @@
                                 </p>
                             </div>
 
+                            @if(session('status'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    <i class="bi bi-info-circle me-1"></i>
+                                    {{ __('A password reset link has been sent to your email address') }} 
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            @endif
+
                             <!-- Start Log in form --> 
                             <form method="post" action="{{ route('password.email') }}" class="row g-3" novalidate>
                                 @csrf
@@ -34,12 +50,15 @@
                                     <label for="email" class="form-label">
                                         {{ __('Email') }} 
                                     </label>
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" required autocomplete="email" autofocus>
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
+                                    <div class="input-group has-validation">
+                                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" required autocomplete="email" autofocus>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
                                 </div>
 
                                 <div class="col-12">
@@ -52,8 +71,11 @@
                                 </div>
 
                                 <div class="col-12">
-                                    <button type="submit" class="btn btn-primary w-100" name="submit">
-                                        {{ __('Submit') }} 
+                                    <button type="submit" class="btn btn-primary w-100" id="submit">
+                                        <i class="loading-icon fa fa-spinner fa-spin hide"></i>
+                                        <span class="btn-txt">
+                                            {{ __('Send Password Reset Link') }} 
+                                        </span>
                                     </button>
                                 </div> 
 
@@ -65,4 +87,30 @@
             </div>
         </div>
     </section>     
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+
+        const submit = document.querySelector("#submit");
+        const loadingIcon = document.querySelector(".loading-icon");
+        const buttonText = document.querySelector(".btn-txt");
+
+        submit.addEventListener("click", function () {
+
+            const list = loadingIcon.classList;
+            list.remove("hide");
+            // submit.setAttribute("disabled", "");     Alternate code: submit.disabled = true;
+            buttonText.textContent = "Please wait";
+
+            /*
+            setTimeout(() => {
+                list.add("hide");
+                submit.removeAttribute("disabled");  // Alternate code: submit.disabled = false;
+                buttonText.textContent = "Log in";
+            }, 3000);
+            */
+        });
+
+    </script>
 @endsection
